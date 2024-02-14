ac5dad5dd00d46a01a71ab51775565f790508b7b
Nicolas Rineau - vscode - int <nicolas.rineau@sanofi.com>
2024-02-14 14:17:03 +0000

    version ewise

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
+aws eks update-kubeconfig --region eu-west-1 --name w-c-eks-f-int
+alias k='kubectl'
+alias kgp='kubectl get pod -n n8n'
+alias kdp='kubectl describe pod -n n8n'
+alias kl='kubectl logs -n n8n'
+alias klf='kubectl logs -n n8n --follow'
+alias kgs='kubectl get svc -n n8n'
+alias kds='kubectl describe svc -n n8n'
+alias kgi='kubectl get ingress -n n8n'
+alias kdi='kubectl describe ingress -n n8n'
+alias kexec='k exec -it -n n8n'
+alias kdelp='kubectl delete pod -n n8n'
+alias ll='ls -artl'
+alias krollout='kubectl rollout restart -n n8n '
+alias kgd='kubectl get Deployment -n n8n'
+alias kdd='kubectl describe Deployment -n n8n'
+alias kapply='kubectl apply -n n8n -f '
+alias kevents='kubectl get events -n n8n '
+alias kpv='kubectl get pv -n n8n '
+alias kpvc='kubectl get pvc -n n8n '
 k apply -f ./namespace.yaml 
 sleep 5
 k apply -f ./n8n-claim0-persistentvolumeclaim.yaml  
